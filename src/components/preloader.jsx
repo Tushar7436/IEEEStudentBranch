9c1e90387b1e5ad4324a18bee6f3d0656cd94e01
Tushar Agarwal <tushar7436@gmail.com>
2024-08-06 17:03:36 +0530

    Preloader added

---
+++ src/components/preloader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../Assets/styles/Preloader.css";
+
+export const PreLoader = () => {
+  return (
+    <div className="load">
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+    </div>
+  );
+};
